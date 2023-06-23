6f3abb806e3c31c448d880367542aab11223acc4
codeCampWithSifat <sayedhossainsifat300@.com>
2023-06-24 00:30:59 +0600

    maximum part done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
 // use all the middleware
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Management Admin!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Management Admin!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-const port: number = 5000;
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("Database connected successfully");
+    console.log('Database connected successfully');
     app.listen(config.port, () => {
       console.log(
         `Practice Management And Admin listening on port ${config.port}`
       );
     });
   } catch (error) {
-    console.log("Database Error", error);
+    console.log('Database Error', error);
   }
 }
 
