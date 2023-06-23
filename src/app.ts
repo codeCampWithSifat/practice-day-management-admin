d01d5600121ee4b2f2ffd262f3d288115e4e228a
codeCampWithSifat <sayedhossainsifat300@.com>
2023-06-23 23:09:37 +0600

    start the project

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application } from "express";
+import cors from "cors";
+const app: Application = express();
+const port: number = 3000;
+
+// use all the middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
